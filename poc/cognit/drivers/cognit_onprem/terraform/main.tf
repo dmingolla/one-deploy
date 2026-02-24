ae8673e18abd38d4d50f39da7bd122db7c1e3e39
Daniele Mingolla <dmingolla@opennebula.io>
2026-02-24 17:46:21 +0100

    Init: Removing custom oneform drivers and fixing ecf deployment Signed-off-by: Daniele Mingolla <dmingolla@opennebula.io>

--- poc/cognit/drivers/cognit_onprem/terraform/main.tf
+++
@@ -1,8 +0,0 @@
-resource "null_resource" "onprem_host" {
-    for_each = toset(var.oneform_onprem_hosts)
-
-    triggers = {
-        instance_id = "onprem-${replace(each.value, ".", "-")}"
-        instance_ip = each.value
-    }
-}
